6c33574b3e33519edf2a9d689c93120dea71760b
anassfiqhi <anassfiqhi@gmail.com>
2022-09-06 13:51:12 +0100

    Adding E2E Test for the main page

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,25 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+/// <reference types="cypress" />
+
+it('shows loading element on scroll', () => {
+  cy.visit('0.0.0.0:3000')
+  cy.get('.ballanim').should('be.visible')
+  cy.get('.ballanim').should('not.be.visible')
+
+  cy.get('.pokemon')
+    .should('have.length', 20)
+    .then((pokemons) => {
+      cy.get('#root').scrollTo('bottom')
+      cy.get('.pokemon').should('have.length', pokemons.length * 2)
+      cy.wait(1000)
+    })
+
+  cy.get('#root').scrollTo('bottom')
+  cy.get('.ballanim').should('be.visible')
+  cy.get('.ballanim').should('not.be.visible')
+  cy.wait(1000)
+
+  cy.get('#root').scrollTo('bottom')
+  cy.get('.ballanim').should('be.visible')
+  cy.get('.ballanim').should('not.be.visible')
+  cy.wait(1000)
 })
