e35f4000cae084de5dbb5077fc369fb64306315c
anassfiqhi <anassfiqhi@gmail.com>
2022-09-06 12:35:34 +0100

    Centering Button

--- src/Components/Pokemon/Pokemon.tsx
+++ src/Components/Pokemon/Pokemon.tsx
@@ -8,7 +8,7 @@ const Pokemon = ({ pokemon, onClick }: PokemonProps) => {
     return (
         <>
             <div className='pokemon flex flex-col justify-center items-center w-64 md:w-72 cursor-pointer mx-auto my-5'>
-                <button className='hover:bg-slate-200 rounded-md p-5' onClick={() => onClick()} key={pokemon.name}>
+                <button className='hover:bg-slate-200 rounded-md p-5 flex flex-col justify-center items-center' onClick={() => onClick()} key={pokemon.name}>
                     <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`} alt={pokemon.name} />
                     <p>{pokemon.name}</p>
                 </button>
